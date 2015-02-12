beb2d495db3aa2425430c2ddfc3c306e9dc6f1a9
Ngoguey42 <ngoguey@student.42.fr>
2015-02-12 05:05:53 +0100

    splitting emacs configs into .el files

---
+++ bindings.el
@@ -0,0 +1,28 @@
+
+(global-set-key (kbd "DEL") 'backward-delete-char)
+(setq-default c-backspace-function 'backward-delete-char)
+
+
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+  
+(global-set-key (kbd "<kp-7>") "\C-a\C- \C-n\M-w\C-y\C-p")
+(global-set-key (kbd "<kp-8>") 'replace-string)
+(global-set-key (kbd "<kp-9>") 'goto-line)
+
+(global-set-key (kbd "<kp-1>") "\C-a\C-k\177\C-a\C-n")
+(global-set-key (kbd "<kp-4>") "\C-y")
+(global-set-key (kbd "<kp-0>") "\C-a\C-m\C-b\C-y\C-a")
+(global-set-key (kbd "<kp-5>") 'yank-pop)
+
+(global-set-key (kbd "<kp-3>") 'toggle-comment-on-line)
+(global-set-key (kbd "<kp-6>") 'comment-or-uncomment-region)
+(global-set-key (kbd "<kp-5>") "\M-;")
+
+
+(global-set-key (kbd "<kp-add>") 'forward-paragraph)
+(global-set-key (kbd "<kp-subtract>") 'backward-paragraph)
+(global-set-key (kbd "<kp-enter>") "\C-m")
