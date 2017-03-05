307b9784fda10ed046a738dd429560361e6dd50d
Ngoguey42 <ngoguey@student.42.fr>
2017-03-05 15:19:52 +0100

    Update dotemacs.el

--- dotemacs.el
+++ dotemacs.el
@@ -1,18 +1,19 @@
-;******************************************************************************;
-;                                                                              ;
-;                                                         :::      ::::::::    ;
-;    .emacs                                             :+:      :+:    :+:    ;
-;                                                     +:+ +:+         +:+      ;
-;    By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+         ;
-;                                                 +#+#+#+#+#+   +#+            ;
-;    Created: 2015/02/09 15:34:40 by ngoguey           #+#    #+#              ;
-;    Updated: 2015/11/15 09:04:02 by ngoguey          ###   ########.fr        ;
-;                                                                              ;
-;******************************************************************************;
+;;****************************************************************************;;
+;;                                                                            ;;
+;;                                                        :::      ::::::::   ;;
+;;   .emacs                                             :+:      :+:    :+:   ;;
+;;                                                    +:+ +:+         +:+     ;;
+;;   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        ;;
+;;                                                +#+#+#+#+#+   +#+           ;;
+;;   Created: 2015/02/09 15:34:40 by ngoguey           #+#    #+#             ;;
+;;   Updated: 2017/03/05 15:18:44 by ngoguey          ###   ########.fr       ;;
+;;                                                                            ;;
+;;****************************************************************************;;
 
 ;; (setq config_files "~/configurations/")
 ;; (defvar confPath "~/configurations")
-(defvar confPath "/Volumes/Storage/goinfre/ngoguey/configurations")
+(defvar confPath (getenv "NGOCONF_PATH"))
+;; (defvar confPath "/Volumes/Storage/goinfre/ngoguey/configurations")
 ;; (setq load-path (append (list nil config_files) load-path))
 (setq load-path (append (list nil confPath) load-path))
 (load "my_config.el")
